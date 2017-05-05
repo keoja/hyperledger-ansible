f75444f20fb52a0cdb463b37b30806084459ddef
Daniel Ford <dan@keoja.com>
2017-05-05 16:21:49 -0400

    initial commit of docker_swarm_node_status.py

---
+++ playbooks/library/docker_swarm_node_status.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+DOCUMENTATION = '''
+---
+module: docker_swarm_node_status
+short_description:
+    - Get the status of a docker swarm node.
+description:
+    - A module for extracting the current status of a host with respect to its status as a node in a docker swarm.
+author: Keoja LLC
+'''
+
+EXAMPLES = '''
+- name: Query the host for its status as a docker swarm node
+  docker_swarm_node_status:
+  register: swarm_status
+'''
+
+import json
+import docker
+
+docker_py_installed = False
+
+# Check to see if docker-py is installed
+# See: https://docker-py.readthedocs.io/en/stable/client.html
+try:
+    from docker import Client
+    docker_py_installed=True
+except:
+    try:
+        from docker import APIClient as Client
+        docker_py_installed=True
+    except:
+        docker_py_installed=False
+
+def makeFacts(info):
+    return {'isActive': info['Swarm']['LocalNodeState'] == "active",
+            'numManagers': info['Swarm']['Managers'],
+            'numNodes':  info['Swarm']['Nodes'],
+            }
+
+def main():
+
+    module = AnsibleModule(
+     argument_spec = dict(
+        ),
+        add_file_common_args=True,
+        supports_check_mode=True
+    )
+
+    # Are we good to go?
+    if docker_py_installed:
+       try: 
+         info = Client().info()
+         module.exit_json(changed=True, ansible_facts=makeFacts(info))  
+       except Exception as e:
+         module.fail_json(msg=e.message)
+    else:
+       # No
+       module.fail_json(msg='docker-py not installed')
+        
+# import module snippets
+from ansible.module_utils.basic import *
+if __name__ == '__main__':
+    main()
