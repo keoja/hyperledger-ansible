5fb4a60602c4e6e40fec2e5684def205fc6c15eb
Daniel Ford <dan@keoja.com>
2017-06-30 15:51:29 -0400

    sync

--- bin/fabricconfiguration.sh
+++ bin/fabricconfiguration.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-export ANSIBLE_NOCOWS=1; ansible-playbook --ask-become-pass -i fabrichosts --tags docker playbooks/fabriclauncher.yml
+export ANSIBLE_NOCOWS=1; ansible-playbook --ask-become-pass -i fabrichosts playbooks/fabriclauncher.yml
